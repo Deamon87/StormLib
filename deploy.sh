72faf921693044be77a45442bb5342d3c6958776
Deamon87 <dmitolm@gmail.com>
2016-12-08 17:24:44 +0200

    test3

--- deploy.sh
+++ deploy.sh
@@ -21,12 +21,6 @@ function compileBuild {
      rm -f build/**/*
 }
 
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    exit 0
-fi
-
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
